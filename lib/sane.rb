baf72b379e4d3fc2d34397c63d840950a532fffe
rdp <rogerpack2005@gmail.com>
2009-10-26 15:08:45 -0600

    try not to use backports too much

--- lib/sane.rb
+++ lib/sane.rb
@@ -1,5 +1,6 @@
 # some uber useful gems
 require 'require_all'
-require 'backports'
+require 'backports/tools.rb' # backports
+require 'backports/1.9/io' # File.binread
 require 'hash_set_operators' # not sure why these aren't in core
 require_rel 'sane_ruby' # require all sub files
