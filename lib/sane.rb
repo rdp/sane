1127e7117cf46b7817a6787672da7764ec44e3e3
rdp <rogerpack2005@gmail.com>
2009-11-12 08:14:47 -0700

    add install local

--- lib/sane.rb
+++ lib/sane.rb
@@ -1,8 +1,29 @@
 # some uber useful gems
+require 'rubygems'
 require 'require_all'
 require 'backports/tools.rb' # backports
 require 'backports/1.9/io' # File.binread
 require 'hash_set_operators' # not sure why these aren't in core
 require_rel 'sane' # require all sub files
+
+class Sane
+ def self.install_local!
+   require 'fileutils'
+   require 'rbconfig'
+
+   Dir.chdir File.dirname(__FILE__) do
+     for file in Dir['*'] do
+       FileUtils.cp_r file, Config::CONFIG['sitelibdir'] # copy subdirs, too
+     end
+     installed_sane =  Config::CONFIG['sitelibdir'] + '/' + 'sane.rb'
+     old_contents = File.read installed_sane
+     File.write installed_sane, "require 'faster_rubygems'\n" + old_contents
+     puts 'hardened sane with this gem version (and faster rubygems) to ' + installed_sane
+
+   end
+
+ end
+
+end
 
 
--- sane.gemspec
+++ sane.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{sane}
-  s.version = "0.9.2"
+  s.version = "0.9.3"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Roger Pack"]
-  s.date = %q{2009-11-05}
+  s.date = %q{2009-11-12}
   s.description = %q{Helper methods for ruby to make it easier to work with out of the box--things that are missing from core but should be there}
   s.email = ["rogerdpack@gmail.com"]
   s.extra_rdoc_files = [
