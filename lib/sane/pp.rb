5e5ccc8db267d4f5b514a6ea3fcbbdada9bbab63
rdp <rogerpack2005@gmail.com>
2010-01-19 14:20:56 -0700

    bump gem version

--- lib/sane/pp.rb
+++ lib/sane/pp.rb
@@ -3,7 +3,7 @@
 module Kernel
   private
   def pp(*objs)
-     undef pp # avoid a warning
+     undef :pp # avoid a warning
      require('pp') || load('pp.rb') # allow for them to have loaded pp before sane
      pp(*objs) # use the new method
   end 
