6e2fd002110738ca2d38c7d188f3f462ba438b8f
Roger Pack <rogerpack2005@gmail.com>
2011-04-15 13:00:05 -0700

    Edited lib/sane/hash_hashes.rb via GitHub

--- lib/sane/hash_hashes.rb
+++ lib/sane/hash_hashes.rb
@@ -1,7 +1,7 @@
 # extracted from from http://blog.playlouder.com/2008/05/07/an-interesting-ruby-hash-semantics-gotcha/
 
-if RUBY_VERSION < '1.9' # not sure if this is needed for 1.8.7 or not...
-# see http://kpumuk.info/ruby-on-rails/memo-5-use-ary-uniq-method-carefully-in-ruby and test it :)
+if RUBY_VERSION < '1.8.7'
+# see http://kpumuk.info/ruby-on-rails/memo-5-use-ary-uniq-method-carefully-in-ruby 
 
 class Hash
  def hash
